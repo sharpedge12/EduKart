85a5e5a07f8ccae38b8510a11448d1ee1a01badf
sharpedge12 <75135658+sharpedge12@users.noreply.github.com>
2025-02-21 14:11:57 +0530

    updating frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Header from "../components/Layout/Header";
+import Hero from "../components/Route/Hero/Hero";
+import Categories from "../components/Route/Categories/Categories";
+import BestDeals from "../components/Route/BestDeals/BestDeals";
+import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
+import Events from "../components/Events/Events";
+import Sponsored from "../components/Route/Sponsored";
+import Footer from "../components/Layout/Footer";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+        <Header activeHeading={1} />
+        <Hero />
+        <Categories />
+        <BestDeals />
+        <Events />
+        <FeaturedProduct />
+        <Sponsored />
+        <Footer />
     </div>
   )
 }
