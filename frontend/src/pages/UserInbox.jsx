9e0c653ec86a935810ac7d2dd05f9a2cde0d69ce
sharpedge12 <75135658+sharpedge12@users.noreply.github.com>
2025-02-24 18:08:50 +0530

    updating links

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-const ENDPOINT = "https://edukartsockett-1b5bseevj-sharps-projects-ffb9298c.vercel.app/";
+const ENDPOINT = "https://edukartsockett-1b5bseevj-sharps-projects-ffb9298c.vercel.app:4000";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
