e55cb8731f4b7e356a4801829079b3b91b787be1
sharpedge12 <75135658+sharpedge12@users.noreply.github.com>
2025-02-24 17:54:39 +0530

    update links

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import styles from "../../styles/styles";
 import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const ENDPOINT = "http://localhost:4000/";
+const ENDPOINT = "https://edukartsockett-1b5bseevj-sharps-projects-ffb9298c.vercel.app/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
